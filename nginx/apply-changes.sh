ca66893f64135980ee71b0111fc4390fb44fded3
Matheus Lima <mellomatheuslima@gmail.com>
2025-09-05 16:27:23 -0300

    Try a fix for apply chanegs

--- nginx/apply-changes.sh
+++ nginx/apply-changes.sh
@@ -1,5 +1,5 @@
 # Copy /config/nginx.conf to /etc/nginx/sites-available/default
-sudo cp ./config/nginx.conf /etc/nginx/sites-available/default
+sudo cp ./nginx/config/nginx.conf /etc/nginx/sites-available/default
 
 # Reload nginx
 sudo systemctl reload nginx
