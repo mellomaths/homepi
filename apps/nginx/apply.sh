86b73f13789bb99ba1af4b7602aa74db2427e747
Matheus Lima <mellomatheuslima@gmail.com>
2025-09-05 16:45:14 -0300

    Fixes on glance setup and nginx server

--- apps/nginx/apply.sh
+++ apps/nginx/apply.sh
@@ -1,5 +1,5 @@
 # Copy /config/nginx.conf to /etc/nginx/
-sudo cp ./nginx/config/nginx.conf /etc/nginx/nginx.conf
+sudo cp ./config/nginx.conf /etc/nginx/nginx.conf
 # sudo cp ./nginx/config/nginx.conf /etc/nginx/sites-available/default
 
 # Reload nginx
