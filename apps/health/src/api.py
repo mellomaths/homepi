ed373a4075b5d804114937af4e1bd96d4f3c864b
Matheus Lima <mellomatheuslima@gmail.com>
2025-09-06 20:17:11 -0300

    Update api health check

--- apps/health/src/api.py
+++ apps/health/src/api.py
@@ -4,4 +4,4 @@ from controllers.health_check_controller import router as health_check_api
 
 router = APIRouter(redirect_slashes=False)
 
-router.include_router(health_check_api, prefix='/health', tags=['health'])
+router.include_router(health_check_api, prefix='/check', tags=['health-check'])
