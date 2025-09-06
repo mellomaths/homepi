03a2a4157f61c4ca1c7f74f712c4976e10933bc0
Matheus Lima <mellomatheuslima@gmail.com>
2025-09-06 20:22:40 -0300

    Fix trailing slashes on health

--- apps/health/src/api.py
+++ apps/health/src/api.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 from controllers.health_check_controller import router as health_check_api
 
-router = APIRouter(redirect_slashes=False)
+router = APIRouter()
 
 router.include_router(health_check_api, prefix='/check', tags=['health-check'])
